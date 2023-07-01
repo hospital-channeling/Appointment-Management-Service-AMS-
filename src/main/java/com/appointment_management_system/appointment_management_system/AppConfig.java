9c9215c09d4fb7922c0d35671464ab30ff892d23
hnsemage <hnsemage@students.nsbm.ac.lk>
2023-07-02 00:18:39 +0530

    New changes in files and add new files

---
+++ src/main/java/com/appointment_management_system/appointment_management_system/AppConfig.java
@@ -0,0 +1,13 @@
+package com.appointment_management_system.appointment_management_system;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
--- src/main/java/com/appointment_management_system/appointment_management_system/AppointmentManagementSystemApplication.java
+++ src/main/java/com/appointment_management_system/appointment_management_system/AppointmentManagementSystemApplication.java
@@ -2,8 +2,10 @@ package com.appointment_management_system.appointment_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AppConfig.class)
 public class AppointmentManagementSystemApplication {
 
 	public static void main(String[] args) {
