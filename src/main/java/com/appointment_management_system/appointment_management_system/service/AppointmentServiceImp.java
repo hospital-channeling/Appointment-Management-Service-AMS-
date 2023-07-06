24e83efc88ae73766ac578f0c3a3acbeb0b0e341
hnsemage <hnsemage@students.nsbm.ac.lk>
2023-07-06 15:43:36 +0530

    Changed file name in service directory and update other related files

--- src/main/java/com/appointment_management_system/appointment_management_system/service/AppointmentServiceImp.java
+++ src/main/java/com/appointment_management_system/appointment_management_system/service/AppointmentServiceImp.java
@@ -1,17 +1,64 @@
 package com.appointment_management_system.appointment_management_system.service;
 
 import com.appointment_management_system.appointment_management_system.entity.Appointment;
+import com.appointment_management_system.appointment_management_system.repository.IAppointmentRepo;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface AppointmentServiceImp {
+@Service
+@Transactional
+public class AppointmentServiceImp implements IAppointmentService {
 
-    Appointment createAppointment(Appointment appointment);
-    void deleteAppointment(int id);
+    @Autowired
+    private IAppointmentRepo appointmentRepo;
+    private EntityManager entityManager;
 
-    Appointment updateAppointment(int id,Appointment appointment);
+    @Override
+    public Appointment createAppointment(Appointment appointment) {
+        appointment.setAppStatus("Not Confirmed");
+        return appointmentRepo.save(appointment);
+    }
 
-    Appointment getAppointmentByID(int id);
+    @Override
+    public void deleteAppointment(int id) {
+        appointmentRepo.deleteById(id);
+    }
 
-    List<Appointment> getAllAppointment();
+    @Override
+    public Appointment updateAppointment(int id, Appointment appointment) {
+        Optional<Appointment> exsistingAppointment = appointmentRepo.findById(id);
+        Appointment updatedAppointment = exsistingAppointment.get();
+        if (exsistingAppointment.isPresent()) {
+            updatedAppointment.setPatientName(appointment.getPatientName());
+            updatedAppointment.setDoctorName(appointment.getDoctorName());
+            updatedAppointment.setAppDate(appointment.getAppDate());
+            updatedAppointment.setAppStatus(appointment.getAppStatus());
+        } else {
+            System.out.println("No appointment found");
+        }
+        return appointmentRepo.save(updatedAppointment);
+    }
+
+    @Override
+    public List<Appointment> getAllAppointment() {
+
+        return (List<Appointment>) appointmentRepo.findAll();
+    }
+
+    @Override
+    public Appointment getAppointmentByID(int id) {
+        if (appointmentRepo.findById(id).isPresent()) {
+            return appointmentRepo.findById(id).get();
+        } else {
+            System.out.println("User Not Found" + id);
+        }
+        return null;
+    }
+
 }
+
---
+++ src/main/java/com/appointment_management_system/appointment_management_system/service/IAppointmentService.java
@@ -0,0 +1,17 @@
+package com.appointment_management_system.appointment_management_system.service;
+
+import com.appointment_management_system.appointment_management_system.entity.Appointment;
+
+import java.util.List;
+
+public interface IAppointmentService {
+
+    Appointment createAppointment(Appointment appointment);
+    void deleteAppointment(int id);
+
+    Appointment updateAppointment(int id,Appointment appointment);
+
+    Appointment getAppointmentByID(int id);
+
+    List<Appointment> getAllAppointment();
+}
